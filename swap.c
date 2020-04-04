5024df3d16061cdf5770b9cd911e73fa2510c194
MFathy14 <mfathyhamed14@gmail.com>
2020-04-04 18:20:40 +0200

    Third Commit- Files Editing

--- swap.c
+++ swap.c
@@ -2,8 +2,8 @@
 void swp(int* num1, int* num2){
 
    if(*num1 != *num2){
-        int temp = *num1;
+        int Temp = *num1;
         *num1 = *num2;
-        *num2 = temp;
+        *num2 = Temp;
     }
 }
